196cd4f4a4abd50f8350d938ea6085505604d2e9
NatapasL <natapas.lue@gmail.com>
2024-07-27 03:19:05 +0700

    Add text style

---
+++ src/styles/text.tsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+export const Header1 = styled.h1`
+  font-family: 'Sarabun-SemiBold';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 64px;
+  line-height: 102px;
+`;
+
+export const SubHeader1 = styled.span`
+  font-family: 'Sarabun-SemiBold';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 40px;
+`;
+
+export const Title1 = styled.span`
+  font-family: 'Sarabun-ExtraBold';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 52px;
+`;
+
+export const Title2 = styled.span`
+  font-family: 'Sarabun-SemiBold';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 40px;
+`;
+
+export const Title3 = styled.span`
+  font-family: 'Sarabun';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 28px;
+`;
+
+export const Small1 = styled.span`
+  font-family: 'Sarabun';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+`;
+
+export const Small2 = styled.span`
+  font-family: 'Sarabun-Medium';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 20px;
+`;
+
+export const Body1 = styled.span`
+  font-family: 'Sarabun';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 28px;
+`;
+
+export const Body2 = styled.span`
+  font-family: 'Sarabun';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+`;
