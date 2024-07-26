b4b4d96576684bf8239d78e939ee7cdaac034d90
NatapasL <natapas.lue@gmail.com>
2024-07-26 13:27:57 +0700

    Use input container to input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,10 @@
-import { ReactElement } from "react";
+import { ReactElement, useMemo } from "react";
 import { useFormContext } from "react-hook-form";
+import styled from "styled-components";
+import { InputContainer } from "./InputContainer";
+
+const REQUIRED_ERROR_MESSAGE = `Please complete this field`;
+const MAX_LENGTH_ERROR_MESSAGE = `Max :maxLength: characters`;
 
 export interface TitleInputProps {
   name: string;
@@ -21,24 +26,32 @@ export const Input = ({
     formState: { errors },
   } = useFormContext();
 
-  console.log(errors);
+  const errorMessage = useMemo((): string => {
+    return errors[name]?.message?.toString() ?? ``;
+  }, [errors[name]?.message]);
+
+  const maxLengthErrorMessage = useMemo(() => {
+    return MAX_LENGTH_ERROR_MESSAGE.replace(/:maxLength:/, `${maxLength}`);
+  }, [maxLength]);
 
   return (
-    <div>
-      <div>{label}</div>
-      <input
+    <InputContainer label={label} errorMessage={errorMessage}>
+      <StyledInput
+        maxLength={maxLength}
         {...register(name, {
           required: {
             value: required ?? false,
-            message: `Please complete this field`,
+            message: REQUIRED_ERROR_MESSAGE,
           },
           maxLength: {
             value: maxLength,
-            message: `This field can't longer than ${maxLength}`,
+            message: maxLengthErrorMessage,
           },
           value: value ?? ``,
         })}
       />
-    </div>
+    </InputContainer>
   );
 };
+
+const StyledInput = styled.input``;
