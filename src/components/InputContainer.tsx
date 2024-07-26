2eb6d6bb6be94bcdc8231b37d89130b3a122b123
NatapasL <natapas.lue@gmail.com>
2024-07-26 13:27:47 +0700

    Add textarea

---
+++ src/components/InputContainer.tsx
@@ -0,0 +1,22 @@
+import { ReactElement, ReactNode } from "react";
+import { ValidationErrorMessage } from "./ValidationErrorMessage";
+
+export interface InputContainer {
+  label: string;
+  errorMessage: string;
+  children: ReactNode | ReactNode[];
+}
+
+export const InputContainer = ({
+  label,
+  errorMessage,
+  children,
+}: InputContainer): ReactElement => {
+  return (
+    <div>
+      <div>{label}</div>
+      {children}
+      {errorMessage ? <ValidationErrorMessage text={errorMessage} /> : <></>}
+    </div>
+  );
+};
---
+++ src/components/Textarea.tsx
@@ -0,0 +1,57 @@
+import { ReactElement, useMemo } from "react";
+import { useFormContext } from "react-hook-form";
+import styled from "styled-components";
+import { InputContainer } from "./InputContainer";
+
+const REQUIRED_ERROR_MESSAGE = `Please complete this field`;
+const MAX_LENGTH_ERROR_MESSAGE = `Max :maxLength: characters`;
+
+export interface DescriptionInput {
+  name: string;
+  label: string;
+  required?: boolean;
+  value?: string;
+  maxLength: number;
+}
+
+export const Textarea = ({
+  label,
+  name,
+  required,
+  value,
+  maxLength,
+}: DescriptionInput): ReactElement => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const errorMessage = useMemo((): string => {
+    return errors[name]?.message?.toString() ?? ``;
+  }, [errors[name]?.message]);
+
+  const maxLengthErrorMessage = useMemo(() => {
+    return MAX_LENGTH_ERROR_MESSAGE.replace(/:maxLength:/, `${maxLength}`);
+  }, [maxLength]);
+
+  return (
+    <InputContainer label={label} errorMessage={errorMessage}>
+      <StyledTextarea
+        maxLength={maxLength}
+        {...register(name, {
+          required: {
+            value: required ?? false,
+            message: REQUIRED_ERROR_MESSAGE,
+          },
+          maxLength: {
+            value: maxLength,
+            message: maxLengthErrorMessage,
+          },
+          value: value ?? ``,
+        })}
+      />
+    </InputContainer>
+  );
+};
+
+const StyledTextarea = styled.textarea``;
