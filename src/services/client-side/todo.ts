dcbba9b3dcc60b5a0f3ca6c3271d0cb384c0040f
NatapasL <natapas.lue@gmail.com>
2024-07-26 19:42:09 +0700

    Update client fetch todo

--- src/services/client-side/todo.ts
+++ src/services/client-side/todo.ts
@@ -1,39 +1,23 @@
-import { deleteData, getData, patchData, postData } from '../../helpers/fetch';
+import { deleteData, patchData, postData } from '../../helpers/fetch';
 import {
   CreateTodoResponse,
   CustomResponse,
   DeleteTodoResponse,
-  GetAllTodoResponse,
   TodoBody,
   UpdateTodoResponse,
 } from '../../types';
 
 const domain = `http://localhost:3000/api`;
 
-export const getAllTodo = async (): Promise<
-  CustomResponse<GetAllTodoResponse> | undefined
-> => {
-  try {
-    console.log(`get all todo`);
-    // const token = cookies().get(`token`)?.value;
-    // console.log(token);
-    // if (!token) throw new Error(`no token`);
-    return await getData(
-      `https://candidate-assignment.neversitup.com`,
-      `/todo`,
-      undefined,
-      `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ii1PMmhIRVZHdmZ5QlBxemZoRXhGIiwiaWF0IjoxNzIxOTg4MDIxLCJleHAiOjE3MjIwNzQ0MjF9.TDZNi-l1jyuyQXPH-wUjNhmVOA27LLNKrkYiOmaYzl0`
-    );
-  } catch (error) {
-    return undefined;
-  }
-};
-
 export const createTodo = async (
   body: TodoBody
 ): Promise<CustomResponse<CreateTodoResponse> | undefined> => {
   try {
-    return await postData(domain, `/todo`, JSON.stringify(body));
+    return await postData({
+      baseUrl: domain,
+      path: `/todo`,
+      body: JSON.stringify(body),
+    });
   } catch (error) {
     return undefined;
   }
@@ -47,7 +31,11 @@ export const updateTodo = async <
   body: Body
 ): Promise<CustomResponse<UpdateTodoResponse<Keys>> | undefined> => {
   try {
-    return await patchData(domain, `/todo/${id}`, JSON.stringify(body));
+    return await patchData({
+      baseUrl: domain,
+      path: `/todo/${id}`,
+      body: JSON.stringify(body),
+    });
   } catch (error) {
     return undefined;
   }
@@ -57,7 +45,7 @@ export const deleteTodo = async (
   id: string
 ): Promise<CustomResponse<DeleteTodoResponse> | undefined> => {
   try {
-    return await deleteData(domain, `/todo/${id}`);
+    return await deleteData({ baseUrl: domain, path: `/todo/${id}` });
   } catch (error) {
     return undefined;
   }
